1c41299ba38cca6d76ca057efec920a7efa04ac3
ShubhamKathane17 <110915671+ShubhamKathane17@users.noreply.github.com>
2025-06-23 20:59:52 +0530

    Update leetcode_338.java

--- leetcode_338.java
+++ leetcode_338.java
@@ -1,5 +1,5 @@
 // 338. Counting Bits
-// optimal - using bit manipulation to count the number of bits in every number from 0 - n
+// brute force - using bit manipulation to count the number of bits in every number from 0 - n
 // tc- O(n log n) - log n in worst case to count the number of bits in the number & n to iterate on 0 - n
 // sc - O(1)
 
@@ -17,6 +17,27 @@ class Solution {
         int[] ans = new int[n+1];
         for(int i = 0; i <= n; i++){
             ans[i] = hammingWeight(i);
+        }
+
+        return ans;
+    }
+}
+
+// optimal - using bit manipulation directly for each number & storing directly instead of calling another function using observation
+// tc - O(n)
+// sc - O(1)
+
+
+class Solution {
+
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                ans[i] = ans[i / 2];
+            } else {
+                ans[i] = ans[i / 2] + 1;
+            }
         }
 
         return ans;
