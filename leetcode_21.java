c92e38f03f6d7a8e05ce9541288f54963c136d47
ShubhamKathane17 <110915671+ShubhamKathane17@users.noreply.github.com>
2025-06-24 11:33:23 +0530

    Update leetcode_21.java

--- leetcode_21.java
+++ leetcode_21.java
@@ -43,3 +43,25 @@ class Solution {
         return h3.next;
     }
 }
+
+// optimal using recursion to merge the listst
+// tc - O(n)
+// sc - O(n)
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null){
+            return list2;
+        }
+        if(list2 == null){
+            return list1;
+        }
+        if(list1.val <= list2.val){
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
+        }
+        else{
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }
+    }
+}
