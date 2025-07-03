33044cfc3c199ff7c49c9c2ac4af36b21cf8a07c
ShubhamKathane17 <110915671+ShubhamKathane17@users.noreply.github.com>
2025-07-03 16:11:05 +0530

    Update leetcode_547.java

--- leetcode_547.java
+++ leetcode_547.java
@@ -79,3 +79,94 @@ class Solution {
         return count;
     }
 }
+
+// using bfs
+// tc - O(V + E)
+// sc - O(V)
+
+class Solution {
+    public void bfs(Map<Integer, List<Integer>> adjList, int src, boolean[] visited) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(src);
+
+        while (!q.isEmpty()) {
+            int front = q.poll();
+            visited[front] = true;
+
+            for (int nbr : adjList.getOrDefault(front, new ArrayList<>())) {
+                if (!visited[nbr]) {
+                    q.add(nbr);
+                }
+            }
+        }
+
+    }
+
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+
+        Map<Integer, List<Integer>> adjList = new HashMap<>();
+
+        for (int u = 0; u < n; u++) {
+            for (int v = 0; v < n; v++) {
+                if (isConnected[u][v] == 1 && u != v) {
+                    adjList.putIfAbsent(u, new ArrayList<>());
+                    adjList.get(u).add(v);
+                }
+            }
+        }
+
+        int count = 0;
+        boolean[] visited = new boolean[n];
+
+        for (int src = 0; src < n; src++) {
+            if (!visited[src]) {
+                bfs(adjList, src, visited);
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
+
+// directly performing on the isConnected matrix
+// tc - O(V^2)
+// sc - O(V) - queue + visited + 
+
+class Solution {
+    int n;
+
+    public void bfs(int[][] isConnected, int src, boolean[] visited) {
+
+        Queue<Integer> q = new LinkedList<>();
+        q.add(src);
+
+        while (!q.isEmpty()) {
+            int front = q.poll();
+            visited[front] = true;
+
+            for (int v = 0; v < n; v++) {
+                if (!visited[v] && isConnected[front][v] == 1) {
+                    q.add(v);
+                }
+            }
+        }
+
+    }
+
+    public int findCircleNum(int[][] isConnected) {
+        n = isConnected.length;
+        int count = 0;
+        boolean[] visited = new boolean[n];
+
+        for (int src = 0; src < n; src++) {
+            if (!visited[src]) {
+                bfs(isConnected, src, visited);
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
